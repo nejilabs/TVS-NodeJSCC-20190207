7d9beee35f0a7675e6ae7d2d82e4eda8a44d50fa
nellyXinwei <nelsonalbajr@yahoo.com>
2020-12-22 18:49:13 +0800

    finished

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer(
+  (req,res)=>{
+    
+    //Build File Path
+    let filePath = path.join(
+      __dirname, 
+      'public',
+      req.url === '/' ? 'index.html' : req.url
+    );
 
-logger.on(
-  'message',
-  data=>console.log('Called Listener', data)
-);
+    //Extension of file
+    let extensionName = path.extname(filePath);
+    //Initial content type
+    let contentType = 'text/html';
 
-logger.log("Hello World")
+    //Check extension and set content type
+    switch(extensionName){
+      case '.js':
+        contentType = 'text/javascript'
+        break;
+      case '.css':
+        contentType = 'text/css'
+        break;
+      case '.json':
+        contentType = 'applications/json';
+        break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    }
+
+    //Read File
+    fs.readFile(filePath,
+      (err,content)=>{
+        if(err){
+          if(err.code == 'ENOENT'){
+            //Page Not Found
+            fs.readFile(
+              path.join(__dirname,'public','404.html'),
+              (err,content)=>{
+                res.writeHead(200,{'content-type':'text/html'});
+                res.end(content,'utf8');
+              } 
+            )
+          } else{
+            //Some other server error
+            res.writeHead(500);
+            res.end(`Server Error ${err.code}`)
+          }
+        } else{
+          //Success
+          res.writeHead(200,{'content-type':contentType});
+          res.end(content,'utf8');
+        }
+      }
+      )
+  }
+)
+
+const PORT = process.env.PORT || 5000;
+server.listen(
+  PORT,
+  ()=>console.log(`Server running on port ${PORT}`)
+)
